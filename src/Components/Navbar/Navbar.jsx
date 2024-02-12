1d40127001e923b634b148fea1e7c8549920b150
Sona <sonavikash733@gmail.com>
2024-02-12 23:09:13 +0530

    Add files via upload

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,1 +1,19 @@
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <img src="" alt="" className="logo" />
+            <ul>
+                <li><Link to="/About">ABOUT</Link></li>
+                <li><Link to="/Events">EVENTS</Link></li>
+                <li><Link to="/Schedule">SCHEDULE</Link></li>
+                <li><Link to="/Workshops">WORKSHOPS</Link></li>
+                <li><Link to="/Contact">CONTACT</Link></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
