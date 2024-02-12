c2d1f1ea77a22b121b8bffede4cfd3ffd74f120a
Sona <sonavikash733@gmail.com>
2024-02-12 23:02:47 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,27 @@
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Login from './Components/Login/Login';
+import Registration from './Components/Registration/Registration';
+import Schedule from './Components/Schedule/Schedule';
+import Workshops from './Components/Workshops/Workshops';
+import AddWorkshop from './Components/Workshops/Add_Workshop';
+import UpdateWorkshop from './Components/Workshops/Update_Workshop';
+import Navbar from './Components/Navbar/Navbar';
 
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="Registration" element={<Registration />} />
+        <Route path="Schedule" element={<Schedule />} />
+        <Route path="Workshops" element={<Workshops />} />
+        <Route path="AddWorkshop" element={<AddWorkshop />} />
+        <Route path="UpdateWorkshop" element={<UpdateWorkshop />} />
+        <Route path="Navbar" element={<Navbar />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
